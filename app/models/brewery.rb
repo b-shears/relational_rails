4a0218a508175ca63468e0e95d0e860663990c84
Bryan Shears <brshears@gmail.com>
2022-07-08 13:59:13 -0600

    Update view & model tests for story1

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,6 @@
 class Brewery < ApplicationRecord
   validates_presence_of :name
+  validates_presence_of :age 
+  validates :pet_friendly, inclusion: [true, false]
   has_many :beers
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe Brewery, type: :model do
   describe "validations" do
-    it { should validate_presence_of :name}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should allow_value(true).for(:pet_friendly) }
+    it { should allow_value(false).for(:pet_friendly) }
   end
   describe 'relationships' do
     it { should have_many :beers }
