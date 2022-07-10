8d28492c464a0d94cef9bf552c8c2f53f918d8e8
Bryan Shears <brshears@gmail.com>
2022-07-09 19:24:58 -0600

    Complete story 6 & 7 model tests & methods

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,14 +1,14 @@
 class Brewery < ApplicationRecord
-  has_many :beers
-  validates_presence_of :name
-  validates_presence_of :age
-  validates :pet_friendly, inclusion: [true, false]
-end
+    has_many :beers
+    validates_presence_of :name
+    validates_presence_of :age
+    validates :pet_friendly, inclusion: [true, false]
 
-def self.order_by_time_created
-  order(created_at: :desc)
-end
+    def self.order_by_time_created
+      order(created_at: :desc)
+    end
 
-def count_of_beers
-  beers.count
+    def count_of_beers
+      beers.count
+    end
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -29,7 +29,6 @@ RSpec.describe Brewery, type: :model do
       new_belgium = Brewery.create!(name: 'New Beligum Brewing', age: 31, pet_friendly: true)
       beer_1 = new_belgium.beers.create!(name: 'La Folie', style: 'Oud Bruin', review_rating: 10, in_stock: false)
       beer_2 = new_belgium.beers.create!(name: 'Fat Tire', style: 'Amber', review_rating: 4, in_stock: true)
-      binding.pry
       expect(new_belgium.count_of_beers).to eq(2)
     end
 end
