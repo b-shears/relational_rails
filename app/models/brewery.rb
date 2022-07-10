6fe8e134d7262ca411c363849ec704667c4a9ef0
Bryan Shears <brshears@gmail.com>
2022-07-09 18:54:31 -0600

    Work on story 6 & 7

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,14 @@
 class Brewery < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :age 
-  validates :pet_friendly, inclusion: [true, false]
   has_many :beers
+  validates_presence_of :name
+  validates_presence_of :age
+  validates :pet_friendly, inclusion: [true, false]
+end
+
+def self.order_by_time_created
+  order(created_at: :desc)
+end
+
+def count_of_beers
+  beers.count
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Brewery, type: :model do
+RSpec.describe Brewery, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -10,4 +10,27 @@ describe Brewery, type: :model do
   describe 'relationships' do
     it { should have_many :beers }
   end
+
+  describe "::order_by_time_created" do
+    it 'orders breweries correctly by most recently created' do
+      # Beer.destroy_all
+      # Brewery.destroy_all
+      new_belgium = Brewery.create!(name: 'New Beligum Brewing', age: 31, pet_friendly: true, created_at: 15.seconds.ago)
+      odell = Brewery.create!(name: 'Odell Brewing', age: 33, pet_friendly: true, created_at: 3.seconds.ago)
+      funkwerks = Brewery.create!(name: 'Funkwerks', age: 12, pet_friendly: true, created_at: 10.seconds.ago)
+      aleworks = Brewery.create!(name: 'Loveland Aleworks', age: 10, pet_friendly: false, created_at: 1.seconds.ago)
+      # binding.pry
+      expect(Brewery.order_by_time_created).to eq([aleworks, odell, funkwerks, new_belgium])
+    end
+  end
+
+  describe 'count_of_beers' do
+    it 'returns the count of beers at a brewery' do
+      new_belgium = Brewery.create!(name: 'New Beligum Brewing', age: 31, pet_friendly: true)
+      beer_1 = new_belgium.beers.create!(name: 'La Folie', style: 'Oud Bruin', review_rating: 10, in_stock: false)
+      beer_2 = new_belgium.beers.create!(name: 'Fat Tire', style: 'Amber', review_rating: 4, in_stock: true)
+      binding.pry
+      expect(new_belgium.count_of_beers).to eq(2)
+    end
+end
 end
