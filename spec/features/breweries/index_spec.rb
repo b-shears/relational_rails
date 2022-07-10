58a7eed123475ecb7fcc5e30f9f55a263b1c612d
Bryan Shears <brshears@gmail.com>
2022-07-10 12:00:45 -0600

    Add feature tests & view for story_6

--- spec/features/breweries/index_spec.rb
+++ spec/features/breweries/index_spec.rb
@@ -7,32 +7,29 @@ RSpec.describe "breweries index page", type: :feature do
     expect(page).to have_content(new_belgium.name)
   end
 
-  xit 'orders the index by most recently create_at brewery' do
-    new_belgium = Brewery.create!(name: 'New Beligum Brewing', age: 31, pet_friendly: true, created_at: 15.seconds.ago)
+  it 'orders the index by most recently create_at brewery' do
+    new_belgium = Brewery.create!(name: 'New Belgium Brewing', age: 31, pet_friendly: true, created_at: 15.seconds.ago)
     odell = Brewery.create!(name: 'Odell Brewing', age: 33, pet_friendly: true, created_at: 3.seconds.ago)
     funkwerks = Brewery.create!(name: 'Funkwerks', age: 12, pet_friendly: true, created_at: 10.seconds.ago)
     aleworks = Brewery.create!(name: 'Loveland Aleworks', age: 10, pet_friendly: false, created_at: 1.seconds.ago)
     visit '/breweries'
-    # save_and_open_page
-    # within '#brewery-0' do
-      expect(page).to have_content('New Belgium Brewing')
-    # end
+    save_and_open_page
 
-    # within '#brewery-1' do
-      expect(page).to have_content('Odell')
-    # end
+    within '#brewery-0' do
+      expect(page).to have_content('Loveland Aleworks')
+    end
 
-    # within '#brewery-2' do
+    within '#brewery-1' do
+      expect(page).to have_content('Odell Brewing')
+    end
+
+    within '#brewery-2' do
       expect(page).to have_content('Funkwerks')
-    # end
+    end
 
-    # within '#brewery-3' do
-      # expect(page).to have_content('Loveland Aleworks')
-    # end
-
-    # within '#new_belgium' do
-      # expect(page).to have_content('New Belgium Brewing')
-    # end
+    within '#brewery-3' do
+      expect(page).to have_content('New Belgium Brewing')
+    end
   end
 
  #  As a visitor
