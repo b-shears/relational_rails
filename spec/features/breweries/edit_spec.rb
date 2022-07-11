c13d9235cf44f9bff39599afa9456d8b3da3f328
Bryan Shears <brshears@gmail.com>
2022-07-11 15:20:48 -0600

    Add feature tests & mvc for story 12 (edit & update)

--- spec/features/breweries/edit_spec.rb
+++ spec/features/breweries/edit_spec.rb
@@ -9,20 +9,28 @@ require 'rails_helper'
 # Then a `PATCH` request is sent to '/parents/:id',
 # the parent's info is updated,
 # and I am redirected to the Parent's Show page where I see the parent's updated info
-RSpec.describe 'Update Brewery' do
+RSpec.describe 'Edit Brewery' do
   describe 'As a vistor' do
     describe 'When I vist the breweries show page' do
-      it 'can update a brewery' do
-        new_belgium= Brewery.create(name: 'New Belgium Brewing')
+      it 'can link to edit brewery' do
+        new_belgium = Brewery.create!(name: 'New Belgium Brewing', age: 31, pet_friendly: true)
+
         visit "/breweries/#{new_belgium.id}"
-        click_link 'Update Brewery'
+
+        click_link "Update Brewery"
 
         expect(current_path).to eq("/breweries/#{new_belgium.id}/edit")
+      end
 
+      it 'can update a brewery' do
+        new_belgium = Brewery.create!(name: 'New Belgium Brewing', age: 31, pet_friendly: true)
+        visit "/breweries/#{new_belgium.id}/edit"
         fill_in 'Name', with: 'New Belgium Brewing Company'
-        click_on 'Update Brewery'
+        fill_in 'Age', with: 31
+        fill_in 'pet_friendly', with: true
+        click_on "Update Brewery"
 
-        expect(current_path).to eq("/breweries")
+        expect(current_path).to eq("/breweries/#{new_belgium.id}")
         expect(page).to have_content('New Belgium Brewing Company')
       end
     end
