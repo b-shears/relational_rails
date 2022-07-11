b9ca5228486acb5d3c04f3a709c059fdce7e5d49
Bryan Shears <brshears@gmail.com>
2022-07-10 20:52:00 -0600

    Add edit_spec for story_12

---
+++ spec/features/breweries/edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+# As a visitor
+# When I visit a parent show page
+# Then I see a link to update the parent "Update Parent"
+# When I click the link "Update Parent"
+# Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+# When I fill out the form with updated information
+# And I click the button to submit the form
+# Then a `PATCH` request is sent to '/parents/:id',
+# the parent's info is updated,
+# and I am redirected to the Parent's Show page where I see the parent's updated info
+RSpec.describe 'Update Brewery' do
+  describe 'As a vistor' do
+    describe 'When I vist the breweries show page' do
+      it 'can update a brewery' do
+        new_belgium= Brewery.create(name: 'New Belgium Brewing')
+        visit "/breweries/#{new_belgium.id}"
+        click_link 'Update Brewery'
+
+        expect(current_path).to eq("/breweries/#{new_belgium.id}/edit")
+
+        fill_in 'Name', with: 'New Belgium Brewing Company'
+        click_on 'Update Brewery'
+
+        expect(current_path).to eq("/breweries")
+        expect(page).to have_content('New Belgium Brewing Company')
+      end
+    end
+  end
+end
