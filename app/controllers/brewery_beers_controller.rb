effbf63ca1ea3785b4e18d9ecf620e8b221d6a63
Bryan Shears <brshears@gmail.com>
2022-07-11 20:29:36 -0600

    Add views, controller methods(new & create) and routes for story_13

--- app/controllers/brewery_beers_controller.rb
+++ app/controllers/brewery_beers_controller.rb
@@ -1,6 +1,22 @@
 class BreweryBeersController < ApplicationController
   def index
-    brewery = Brewery.find(params[:brewery_id])
-    @beers = brewery.beers
+    @brewery = Brewery.find(params[:brewery_id])
+    @beers = @brewery.beers
+  end
+
+  def new
+    @brewery = Brewery.find(params[:brewery_id])
+  end
+
+  def create
+    @brewery = Brewery.find(params[:brewery_id])
+    @brewery.beers.create(brewery_params)
+    redirect_to
+    "/breweries/#{@brewery.id}/beers"
+  end
+
+  private
+  def brewery_params
+    params.permit(:name, :style, :review_rating, :in_stock)
   end
 end
