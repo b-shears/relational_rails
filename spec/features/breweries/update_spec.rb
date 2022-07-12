b03be1e905d69d753fc085f9067108273c69ec44
Bryan Shears <brshears@gmail.com>
2022-07-11 22:58:15 -0600

    Work on view & routes for story_17

--- spec/features/breweries/update_spec.rb
+++ spec/features/breweries/update_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe 'Update from Brewery Index' do
 # When I click the link
 # I should be taken to that parents edit page where I can update its information just like in User Story 4
 
-  it 'can update a brewery from the brewery index' do
+  it 'can update a brewery in the brewery index by clicking on a link' do
     new_belgium = Brewery.create!(name: 'New Beligum Brewing', age: 31, pet_friendly: true)
     visit "/breweries"
 
-    click_link "Update Brewery Info"
-    expect(current_path).to eq("/breweries/#{brewery.id}/edit")
+    click_link "Edit Brewery"
+    expect(current_path).to eq("/breweries/#{@brewery.id}/edit")
   end
 
 end
